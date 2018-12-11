47984f33e95cb146d2aac9c930440083ce668ad7
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2018-12-11 14:03:42 +0100

    reloading file without copy-pasting

---
+++ src/wrapper.js
@@ -0,0 +1,14 @@
+function loadScript(url) {
+    (function (d, s) { s = d.createElement('script'); s.src = url; (d.head || d.documentElement).appendChild(s) })(document);
+}
+
+loadScript('https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.js');
+
+function refreshIt() {
+    var url = 'http://127.0.0.1:8887/conquerorSpy.js';
+    loadScript(url);
+}
+
+setTimeout(function() {
+    refreshIt();
+}, 2000)
