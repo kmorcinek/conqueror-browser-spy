a2b190e0353f7a5120896bdb60347018a9c9cfc7
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2018-12-11 20:16:55 +0100

    Fix bug with NaN turn

--- src/conquerorSpy.js
+++ src/conquerorSpy.js
@@ -46,10 +46,15 @@ function getCountryDetails(countryName)
 // < image id = "prod_crete" class="prod" height = "12" width = "12" x = "738.4375" y = "659" xlink:href = "../common/images/icon_unit.png" visibility = "inherit" transform = "translate(242.56000000000006,213.76)" />
 // icon_farm.png, icon_unit.png, gold, culture, castle_kind, diplomat,
 
-var lastTurn = 0;
+var lastTurn = NaN;
 
 function refreshTurn() {
     var turn = getTurn();
+
+    if (isNaN(turn)) {
+        return;
+    }
+
     if (turn !== lastTurn) {
         lastTurn = turn;
         console.log("New turn: ", lastTurn);
