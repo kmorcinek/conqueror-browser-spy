2122d031850116711bcc51f8d45e47fb1fdca75c
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2019-11-24 21:46:23 +0100

    At turn 11 2+1 province is developing #13

--- src/ProvinceHistoryChecker.ts
+++ src/ProvinceHistoryChecker.ts
@@ -3,10 +3,19 @@ import { Province } from "./Province";
 
 export class ProcinceHistoryChecker {
   checkHistory(history: Province[]): string | null {
-    if (history.length === 0) {
+    if (history.length === 0 || history.length === 1) {
       return null;
     }
 
+    const result = this.checkHistory11turn2farmsPlus1Developing(history);
+    if (result !== null) {
+      return result;
+    }
+
+    return this.checkHistory11turn3farmsDeveloping(history);
+  }
+
+  checkHistory11turn3farmsDeveloping(history: Province[]): string | null {
     // population 3 is longer than x (5?) => developing
     // -start from last one
     const last = history[history.length - 1];
@@ -17,6 +26,29 @@ export class ProcinceHistoryChecker {
       for (let i = history.length - 2; i > -1; i--) {
         const current = history[i];
         if (current.farms === 3 && last.resources === 0 && lastSoldiersCount <= current.soldiers) {
+          counter++;
+        }
+      }
+
+      if (counter > 5) {
+        return last.name + " is developing";
+      }
+    }
+
+    return null;
+  }
+
+  checkHistory11turn2farmsPlus1Developing(history: Province[]): string | null {
+    // population 3 is longer than x (5?) => developing
+    // -start from last one
+    const last = history[history.length - 1];
+    const lastSoldiersCount = last.soldiers;
+
+    if (last.farms === 2 && last.resources === 1 && last.culture === Culture.Primitive) {
+      let counter = 0;
+      for (let i = history.length - 2; i > -1; i--) {
+        const current = history[i];
+        if (current.farms === 2 && last.resources === 1 && lastSoldiersCount <= current.soldiers) {
           counter++;
         }
       }
--- tests/ProvinceHistoryChecker.test.ts
+++ tests/ProvinceHistoryChecker.test.ts
@@ -28,4 +28,20 @@ describe("ProcinceHistoryChecker", () => {
     // tslint:disable-next-line: no-unused-expression
     expect(message).to.equal("poland is developing");
   });
+
+  it("suggest developing with 2 population and 1 resource at 11 turn", () => {
+    const sut = new ProcinceHistoryChecker();
+    const provinceFactory = new ProvinceFactory();
+    provinceFactory.farms = 2;
+    provinceFactory.resources = 1;
+    provinceFactory.culture = Culture.Primitive;
+    const province = provinceFactory.build();
+    const history: Province[] = [];
+    for (let i = 0; i < 7; i++) {
+      history.push(province);
+    }
+    const message = sut.checkHistory(history);
+    // tslint:disable-next-line: no-unused-expression
+    expect(message).to.equal("poland is developing");
+  });
 });
