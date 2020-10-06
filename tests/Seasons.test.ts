488ba98b854f6abf3897894a2fca46c7950130d7
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2020-10-06 22:36:42 +0200

    tests: add Seasons UT

---
+++ tests/Seasons.test.ts
@@ -0,0 +1,17 @@
+import { expect } from "chai";
+import { Season } from "../src/Season";
+import { Seasons } from "../src/Seasons";
+
+describe("SeasonsTest", () => {
+  it("should parse Spring", () => {
+    expect(Seasons.parseSeason(1)).to.equal(Season.Spring);
+    expect(Seasons.parseSeason(5)).to.equal(Season.Spring);
+    expect(Seasons.parseSeason(9)).to.equal(Season.Spring);
+  });
+
+  it("should parse Autumn", () => {
+    expect(Seasons.parseSeason(3)).to.equal(Season.Autumn);
+    expect(Seasons.parseSeason(7)).to.equal(Season.Autumn);
+    expect(Seasons.parseSeason(11)).to.equal(Season.Autumn);
+  });
+});
