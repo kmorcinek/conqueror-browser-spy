694f6c5c59eda5a8b786c1edb87e40db8411c321
Krzysztof Morcinek <krzysztof.morcinek@kitopi.com>
2022-01-16 11:09:05 +0100

    chore: #55 reminder about tagging in git

--- build_deploy.sh
+++ build_deploy.sh
@@ -3,3 +3,8 @@
 sh ./build.sh &&
 
 aws s3 cp lib/output.js s3://krzysztof.morcinek.conquerorgame/output.js --acl public-read --profile s3_conqueror_uploader
+
+# when extracting version will be possible then taging can be automated
+echo "remember to run tagging:
+git tag v1.x
+git push origin --tags"
