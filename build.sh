f27bee1f7e875169b9169acd37e4ff1ddc261c0b
Krzysztof Morcinek <krzysztof.morcinek@kitopi.com>
2022-01-16 11:03:55 +0100

    chore: split build_deploy.sh to separate scripts

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+npm run lint &&
+npm test &&
+npm run browserify
--- build_deploy.sh
+++ build_deploy.sh
@@ -1,4 +1,5 @@
-npm run lint &&
-npm test &&
-npm run browserify &&
+#!/bin/bash
+
+sh ./build.sh &&
+
 aws s3 cp lib/output.js s3://krzysztof.morcinek.conquerorgame/output.js --acl public-read --profile s3_conqueror_uploader
