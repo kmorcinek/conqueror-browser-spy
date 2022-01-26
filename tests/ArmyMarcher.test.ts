0bc1f9691399ae1e28ee572246b34e79f288ceb7
Krzysztof Morcinek <krzysztof.morcinek@kitopi.com>
2022-01-26 23:28:25 +0100

    feat: #56 Move to empty neutral neighbor (regardless opponent is 2 provinces away)

--- tests/ArmyMarcher.test.ts
+++ tests/ArmyMarcher.test.ts
@@ -80,13 +80,13 @@ describe("ArmyMarcher", () => {
     };
 
     const battleProvinceNeighborhoods: IBattleProvinceNeighborhoods = {
-      getManyPathWithDistance2: () => [],
+      getManyPathWithDistance2: () => [[middleTargetNeutralProvince, targetProvince]],
       // tslint:disable: no-empty
       recreateNextTurn: () => {},
       getOwnedProvinces: () => [],
       getByName: () => middleTargetNeutralProvince,
       getClosestNotConqueredNeighbors: () => [],
-      getDistance: () => 0,
+      getDistance: () => 2,
       getPath: () => [middleTargetNeutralProvince.name, targetProvince.name],
       // tslint:enable: no-empty
     };
@@ -96,6 +96,101 @@ describe("ArmyMarcher", () => {
     sut.marchToTarget(sourceProvince, targetProvince);
 
     expect(result).to.equal(undefined);
+  });
+
+  it("should marchToTarget move army through middle neutral target when neutral province is without soldiers", () => {
+    const provinceFactory = new ProvinceFactory();
+    provinceFactory.name = "natolia";
+    provinceFactory.soldiers = 9;
+    const sourceProvince = new BattleProvince(provinceFactory.build(), ProvinceOwner.Me);
+
+    provinceFactory.name = "syria";
+    provinceFactory.soldiers = 0;
+    const middleTargetNeutralProvince = new BattleProvince(
+      provinceFactory.build(),
+      ProvinceOwner.Neutral
+    );
+
+    provinceFactory.name = "palestine";
+    provinceFactory.soldiers = 3;
+    const targetProvince = new BattleProvince(provinceFactory.build(), ProvinceOwner.Opponent);
+
+    let result: ArmyMove | undefined;
+    const armyMovesRecorder: IArmyMovesRecorder = {
+      // tslint:disable: no-empty
+      addMove: (armyMove: ArmyMove) => {
+        result = armyMove;
+      },
+      isFull: () => false,
+      getArmyMoves: () => [],
+      clearMoves: () => {},
+      // tslint:enable: no-empty
+    };
+
+    const battleProvinceNeighborhoods: IBattleProvinceNeighborhoods = {
+      getManyPathWithDistance2: () => [[middleTargetNeutralProvince, targetProvince]],
+      // tslint:disable: no-empty
+      recreateNextTurn: () => {},
+      getOwnedProvinces: () => [],
+      getByName: () => middleTargetNeutralProvince,
+      getClosestNotConqueredNeighbors: () => [],
+      getDistance: () => 2,
+      getPath: () => [middleTargetNeutralProvince.name, targetProvince.name],
+      // tslint:enable: no-empty
+    };
+
+    const sut = new ArmyMarcher(battleProvinceNeighborhoods, armyMovesRecorder);
+
+    sut.marchToTarget(sourceProvince, targetProvince);
+
+    expect(result).to.not.equal(undefined);
+    expect(result!.source).to.equal(sourceProvince);
+    expect(result!.target).to.equal(middleTargetNeutralProvince);
+  });
+
+  it("should sortByLessSoldiersInNextTarget start from empty province", () => {
+    const provinceFactory = new ProvinceFactory();
+    // for better map reading source province is 'natolia'
+
+    provinceFactory.name = "syria";
+    provinceFactory.soldiers = 8;
+    const middleTargetNeutralProvinceNotEmpty = new BattleProvince(
+      provinceFactory.build(),
+      ProvinceOwner.Neutral
+    );
+
+    provinceFactory.name = "cyprus";
+    provinceFactory.soldiers = 0;
+    const middleTargetNeutralProvinceEmpty = new BattleProvince(
+      provinceFactory.build(),
+      ProvinceOwner.Neutral
+    );
+
+    provinceFactory.name = "palestine";
+    provinceFactory.soldiers = 3;
+    const targetProvince = new BattleProvince(provinceFactory.build(), ProvinceOwner.Opponent);
+
+    {
+      // when order is wrong and needs to be sorted
+      const sorted = ArmyMarcher.sortByLessSoldiersInNextTarget([
+        [middleTargetNeutralProvinceNotEmpty, targetProvince],
+        [middleTargetNeutralProvinceEmpty, targetProvince],
+      ]);
+
+      expect(sorted[0][0]).to.equal(middleTargetNeutralProvinceEmpty);
+      expect(sorted[1][0]).to.equal(middleTargetNeutralProvinceNotEmpty);
+    }
+
+    {
+      // when order is correct and method should not change it
+      const sorted = ArmyMarcher.sortByLessSoldiersInNextTarget([
+        [middleTargetNeutralProvinceEmpty, targetProvince],
+        [middleTargetNeutralProvinceNotEmpty, targetProvince],
+      ]);
+
+      expect(sorted[0][0]).to.equal(middleTargetNeutralProvinceEmpty);
+      expect(sorted[1][0]).to.equal(middleTargetNeutralProvinceNotEmpty);
+    }
   });
 
   it("should marchToTarget search for many path and finally find a way through my own provinces", () => {
