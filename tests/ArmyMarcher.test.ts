5f6137dab1047cd5e89af69c29e34a1d204ca26e
Krzysztof Morcinek <krzysztof.morcinek@kitopi.com>
2022-01-26 23:28:25 +0100

    error in ut

--- tests/ArmyMarcher.test.ts
+++ tests/ArmyMarcher.test.ts
@@ -98,6 +98,57 @@ describe("ArmyMarcher", () => {
     expect(result).to.equal(undefined);
   });
 
+
+  it("should marchToTarget move army through middle neutral target when neutral province is without soldiers", () => {
+    const provinceFactory = new ProvinceFactory();
+    provinceFactory.name = "natolia";
+    provinceFactory.soldiers = 9;
+    const sourceProvince = new BattleProvince(provinceFactory.build(), ProvinceOwner.Me);
+
+    provinceFactory.name = "syria";
+    provinceFactory.soldiers = 0;
+    const middleTargetNeutralProvince = new BattleProvince(
+      provinceFactory.build(),
+      ProvinceOwner.Neutral
+    );
+
+    provinceFactory.name = "palestine";
+    provinceFactory.name = "3";
+    const targetProvince = new BattleProvince(provinceFactory.build(), ProvinceOwner.Opponent);
+
+    let result: ArmyMove | undefined;
+    const armyMovesRecorder: IArmyMovesRecorder = {
+      // tslint:disable: no-empty
+      addMove: (armyMove: ArmyMove) => {
+        result = armyMove;
+      },
+      isFull: () => false,
+      getArmyMoves: () => [],
+      clearMoves: () => {},
+      // tslint:enable: no-empty
+    };
+
+    const battleProvinceNeighborhoods: IBattleProvinceNeighborhoods = {
+      getManyPathWithDistance2: () => [],
+      // tslint:disable: no-empty
+      recreateNextTurn: () => {},
+      getOwnedProvinces: () => [],
+      getByName: () => middleTargetNeutralProvince,
+      getClosestNotConqueredNeighbors: () => [],
+      getDistance: () => 0,
+      getPath: () => [middleTargetNeutralProvince.name, targetProvince.name],
+      // tslint:enable: no-empty
+    };
+
+    const sut = new ArmyMarcher(battleProvinceNeighborhoods, armyMovesRecorder);
+
+    sut.marchToTarget(sourceProvince, targetProvince);
+
+    expect(result).to.not.equal(undefined);
+    expect(result?.source).to.equal(sourceProvince.name);
+    expect(result?.target).to.equal(middleTargetNeutralProvince.name);
+  });
+
   it("should marchToTarget search for many path and finally find a way through my own provinces", () => {
     const provinceFactory = new ProvinceFactory();
     provinceFactory.name = "natolia";
