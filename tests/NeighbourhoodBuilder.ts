ebbbbc1ca2f6ca903e50238ff020982aa5be2a7f
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2022-10-29 17:38:21 +0200

    remove old tests

--- tests/NeighbourhoodBuilder.ts
+++
@@ -1,8 +0,0 @@
-import { BattleProvince } from "../src/ai/BattleProvince";
-
-export class NeighbourhoodBuilder {
-  static connect(firstProvince: BattleProvince, secondProvince: BattleProvince) {
-    firstProvince.addNeighbor(secondProvince);
-    secondProvince.addNeighbor(firstProvince);
-  }
-}
