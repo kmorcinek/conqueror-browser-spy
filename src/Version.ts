519f399348f47a62d9f3456b532bc8529851c0ed
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2026-04-29 22:32:13 +0200

    bump version to 1.16 - back to development

--- src/Version.ts
+++ src/Version.ts
@@ -1,7 +1,6 @@
 export class Version {
-  static readonly versionNumber = "v1.15.1";
-  private static readonly versionDescription =
-    "Move to empty neutral neighbor (regardless opponent is 2 provinces away)";
+  static readonly versionNumber = "v1.16";
+  private static readonly versionDescription = "back to development";
 
   static getFullVersion(): string {
     return `${this.versionNumber} - ${this.versionDescription}`;
