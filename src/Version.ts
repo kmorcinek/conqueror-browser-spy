14de62cc350c41e601d4730cdac087de16fcc650
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2026-05-07 19:24:56 +0200

    bump version to 1.17.1 - skip AI on maps with more than 2 players

--- src/Version.ts
+++ src/Version.ts
@@ -1,6 +1,6 @@
 export class Version {
-  static readonly versionNumber = "v1.17";
-  private static readonly versionDescription = "fix security dependencies";
+  static readonly versionNumber = "v1.17.1";
+  private static readonly versionDescription = "skip AI on maps with more than 2 players";
 
   static getFullVersion(): string {
     return `${this.versionNumber} - ${this.versionDescription}`;
