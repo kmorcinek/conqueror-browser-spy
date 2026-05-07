266e88c0bc18c51cf859a807b9461c74417d14fb
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2026-05-07 20:48:28 +0200

    bump version to 1.17 - fix security dependencies

--- src/Version.ts
+++ src/Version.ts
@@ -1,6 +1,6 @@
 export class Version {
-  static readonly versionNumber = "v1.16";
-  private static readonly versionDescription = "back to development";
+  static readonly versionNumber = "v1.17";
+  private static readonly versionDescription = "fix security dependencies";
 
   static getFullVersion(): string {
     return `${this.versionNumber} - ${this.versionDescription}`;
